6acbbbabbf70a0f08b1ba743211891b8e4943247
Kadam36 <swapnilkadam0326@gmail.com>
2024-11-26 16:12:30 +0530

    commit

--- trade_filter/wsgi.py
+++ trade_filter/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for trade_filter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-path = '/home/Kadam36/https://github.com/Kadam36/trading.git'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'trade_filter.settings'
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trade_filter.settings')
+
+application = get_wsgi_application()
